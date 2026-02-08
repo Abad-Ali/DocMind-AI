b8273aa4d5b40062469b58537b89bb413c299274
Abad-Ali <abad02597@gmail.com>
2026-02-08 18:19:29 +0530

    Add verifyemail route

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { editProfile, login, logout, register } from '../controllers/user.controller.js';
+import { editProfile, login, logout, register, verifyEmail } from '../controllers/user.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import upload from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.route('/register').post(register);
+router.route('/verifyemail').post(verifyEmail);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/profile/edit').put(isAuthenticated, upload.single('profilePicture'), editProfile);
