d9355e89a9c4787776a39cfc2ad16de3812a1bc2
Abad-Ali <abad02597@gmail.com>
2026-02-08 12:50:22 +0530

    Update PDF router to handle upload and extraction endpoints

--- backend/routers/pdf.router.js
+++ backend/routers/pdf.router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import isAdmin from '../middlewares/isAdmin.js';
-import { deletePDF, downloadPDF, editPDF, getLatestPDFs, getUploadedPDFs, searchPDF, uploadPDF } from '../controllers/pdf.controller.js';
+import { deletePDF, downloadPDF, editPDF, getLatestPDFs, getUploadedPDFs, searchPDF, testPdfExtraction, uploadPDF } from '../controllers/pdf.controller.js';
 import upload from '../middlewares/multer.js';
 
 const router = express.Router();
@@ -13,5 +13,6 @@ router.route('/getlatest').get(isAuthenticated, getLatestPDFs);
 router.route('/getuploaded').get(isAuthenticated, isAdmin, getUploadedPDFs);
 router.route('/search').get(isAuthenticated, searchPDF);
 router.route('/download/:pdfId').get(isAuthenticated, downloadPDF);
+router.route('/extract/:pdfId').get(isAuthenticated, testPdfExtraction);
 
 export default router
