cf9c986fb3dbc34f06b13e3cb2f48a8a772b01b0
Abad-Ali <abad02597@gmail.com>
2026-02-08 18:21:23 +0530

    Add sendVerificationEmail and sendWelcomeEmail functions

---
+++ backend/utils/Email.js
@@ -0,0 +1,33 @@
+import { transporter } from "./Email.config.js";
+import { Verification_Email_Template, Welcome_Email_Template } from "./EmailTemplate.js";
+
+export const sendVerificationEmail = async(email,verificationCode)=>{
+    try {
+     const response = await transporter.sendMail({
+            from: '"DocMind AI" <no-reply@yourdomain.com>',
+
+            to: email, // list of receivers
+            subject: "Verify your Email", // Subject line
+            text: "Verify your Email", // plain text body
+            html: Verification_Email_Template.replace("{verificationCode}",verificationCode)
+        })
+        console.log('Email send Successfully',response)
+    } catch (error) {
+        console.log('Email error',error)
+    }
+}
+export const sendWelcomeEmail=async(email,name)=>{
+    try {
+     const response=   await transporter.sendMail({
+            from: '"DocMind AI" <no-reply@yourdomain.com>',
+
+            to: email, // list of receivers
+            subject: "Welcome Email", // Subject line
+            text: "Welcome Email", // plain text body
+            html: Welcome_Email_Template.replace("{name}",name)
+        })
+        // console.log('Email send Successfully',response)
+    } catch (error) {
+        console.log('Email error',error)
+    }
+}
