caacbb71aff77c6201b305a1a2ee5de54176212c
Abad-Ali <abad02597@gmail.com>
2026-02-09 18:07:09 +0530

    Add AI questions and summary fields to PDF schema

--- backend/models/pfd.model.js
+++ backend/models/pfd.model.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+const questionSchema = new mongoose.Schema(
+  {
+    question: { type: String, required: true },
+    answer: { type: String, required: true },
+  },
+  { _id: false } // prevents extra _id for each question
+);
+
 const pdfSchema = new mongoose.Schema({
     title:{type:String, required:true, },
     description:{type:String},
@@ -7,6 +15,11 @@ const pdfSchema = new mongoose.Schema({
     fileUrl:{type:String, required:true},
     publicId: {type: String, required: true},
     extractedText:{type: String, required: false},
+    aiSummary: {type: String},
+    aiQuestions: {
+      type: [questionSchema],
+      default: [],
+    },
     uploadedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
