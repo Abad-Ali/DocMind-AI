95eff9f50dd6f2345bc0e2b88448b2e161438708
Abad-Ali <abad02597@gmail.com>
2026-02-10 17:15:48 +0530

    Add router for getting user profile and added another router for changing the password of the authenticated profile

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editProfile, login, logout, register, verifyEmail } from '../controllers/user.controller.js';
+import { changePassword, editProfile, getUserProfile, login, logout, register, verifyEmail } from '../controllers/user.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import upload from '../middlewares/multer.js';
 
@@ -10,5 +10,7 @@ router.route('/verifyemail').post(verifyEmail);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/profile/edit').put(isAuthenticated, upload.single('profilePicture'), editProfile);
+router.route('/getprofile/:userId').get(isAuthenticated, getUserProfile);
+router.route('/changepassword').post(isAuthenticated, changePassword);
 
 export default router
