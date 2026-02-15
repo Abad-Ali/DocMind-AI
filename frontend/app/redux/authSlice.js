fedd9d793f8e5d8612edb216fd558106da2d46e9
Abad-Ali <abad02597@gmail.com>
2026-02-15 17:59:34 +0530

    Setup Redux store, authSlice, provider, and implement login with Redux Toolkit

---
+++ frontend/app/redux/authSlice.js
@@ -0,0 +1,17 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        user: null,
+        // isAuthenticated: false,
+    },
+    reducers:{
+        setAuthUser:(state,action)=>{
+            state.user = action.payload;
+        },
+    }
+});
+
+export const { setAuthUser } = authSlice.actions;
+export default authSlice.reducer;
