cd8661cab18f57f84e462c98b434ee6eb82524d3
Abad-Ali <abad02597@gmail.com>
2026-02-19 19:38:04 +0530

    Created component for recent pdfs

---
+++ frontend/components/RecentPdfs.jsx
@@ -0,0 +1,58 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
+
+const RecentPdfs = () => {
+  const { recentPDF } = useSelector(store => store.recPDF);
+  const router = useRouter();
+
+  return (
+    <div className="text-white flex flex-col gap-7">
+        <div className="flex justify-center">
+            <span className="text-4xl font-bold font-serif">Recently Uploaded PDFs</span>
+        </div>
+
+        <div className="flex justify-center items-center mt-4 mb-7">
+        <div className="grid grid-cols-4 gap-5">
+            {recentPDF.map(pdf => (
+                <div key={pdf.id} onClick={()=>router.push(`pdf/${pdf.id}`)}>
+                    <div className="relative w-full h-full rounded-2xl overflow-hidden">
+                        <img
+                          src={pdf.previewUrl}
+                          width="250px"
+                          height="150px"
+                          className="rounded-2xl"
+                        />
+                        <div className="absolute bottom-0 bg-black/20 backdrop-blur-lg min-w-full min-h-[30vh] rounded-2xl text-black">
+                            <div className="m-3 mt-5">
+                                <div>
+                                    <span className="text-[17px] font-serif font-semibold">Title: </span>
+                                    <span className="text-[17px] font-sans font-semibold">{pdf.title}</span>
+                                </div>
+    
+                                <div>
+                                    <span className="text-[17px] font-serif font-semibold">Description: </span>
+                                    <span className="text-[17px] font-sans font-semibold">{pdf.description}</span>
+                                </div>
+    
+                                <div>
+                                    <span className="text-[17px] font-serif font-semibold">Author: </span>
+                                    <span className="text-[17px] font-sans font-semibold">{pdf.author}</span>
+                                </div>
+    
+                                <div>
+                                    <span className="text-[17px] font-serif font-semibold">Upload by: </span>
+                                    <span className="text-[17px] font-sans font-semibold text-blue-700">{pdf.uploadedBy.username}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default RecentPdfs;
