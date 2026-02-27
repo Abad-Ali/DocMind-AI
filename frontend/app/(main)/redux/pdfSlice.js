80980fd15d7dcb962745ccca8f80b2b12746a737
Abad-Ali <abad02597@gmail.com>
2026-02-28 00:04:39 +0530

    Create uploadedPDFS to store the uploaded pdfs in redux

--- frontend/app/(main)/redux/pdfSlice.js
+++ frontend/app/(main)/redux/pdfSlice.js
@@ -4,13 +4,17 @@ const pdfSlice = createSlice({
     name:"pdf",
     initialState:{
         pdf:null,
+        uploadedPDFS:[],
     },
     reducers:{
         setPDF:(state,action)=>{
             state.pdf = action.payload;
         },
+        setUploadedPDF:(state,action)=>{
+            state.uploadedPDFS = action.payload;
+        },
     }
 });
 
-export const { setPDF } = pdfSlice.actions;
+export const { setPDF, setUploadedPDF } = pdfSlice.actions;
 export default pdfSlice.reducer;
