7d46f0026556087d553101b2c5cf55c51ca2fc57
Abad-Ali <abad02597@gmail.com>
2026-05-06 17:14:55 +0530

    Make Home page responsive

--- frontend/app/(main)/page.js
+++ frontend/app/(main)/page.js
@@ -11,12 +11,12 @@ export default function Home() {
   useGetRecentPdfs();
   const router = useRouter();
   return (
-    <div className='w-full h-full pt-[27vh] pl-64'>
+    <div className='w-full h-full lg:pt-[25vh] pt-[15vh] sm:pt-[14vh] lg:pl-64 [@media(min-width:1023px)_and_(max-width:1350px)]:pt-[10vh]'>
         <div className="text-white flex justify-center items-center">
-          <div className="flex justify-center items-center gap-9">
-            <div className="flex flex-col items-center min-w-lg gap-3">
-              <div className='flex justify-start w-full ml-2'><span className="text-4xl font-bold font-serif text-start">Welcome to </span></div>
-              <div className='flex justify-start w-full ml-36'><span className="text-4xl font-bold font-serif text-blue-700"><Typewriter
+          <div className="flex flex-col lg:flex-row [@media(min-width:1023px)_and_(max-width:1350px)]:flex-col justify-center items-center gap-9">
+            <div className="flex flex-col items-center lg:min-w-lg gap-3">
+              <div className='flex justify-start lg:w-full lg:ml-2'><span className="text-4xl font-bold font-serif text-start">Welcome to </span></div>
+              <div className='flex justify-start lg:w-full lg:ml-36'><span className="text-4xl font-bold font-serif text-blue-700"><Typewriter
                 options={{
                   strings: ['DocMind AI'],
                   autoStart: true,
@@ -27,7 +27,7 @@ export default function Home() {
                 }}
               /></span></div>
     
-              <span className='text-slate-200 w-xl text-center mb-2 italic'>Supercharge your learning with DcoMind AI. Summarize texts, generate questions & answers, and chat directly with PDFs — all powered by AI. Get your notes and Q&A sent straight to your inbox and study smarter,      faster, and easier than ever.</span>
+              <span className='text-slate-200 lg:w-xl text-center mb-2 italic px-3 lg-px-0'>Supercharge your learning with DcoMind AI. Summarize texts, generate questions & answers, and chat directly with PDFs — all powered by AI. Get your notes and Q&A sent straight to your inbox and study smarter,      faster, and easier than ever.</span>
     
               <button onClick={()=>router.push('/explore')} className="bg-blue-700 px-4 py-2.5 rounded-2xl font-semibold cursor-pointer flex gap-1 items-center hover:bg-blue-800 duration-300"><GlobeIcon/> Explore Now For Free <ArrowRight/></button>
             </div>
