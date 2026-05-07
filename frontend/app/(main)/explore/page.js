27a7e194929273dc7f8c50227b66ab58cd947e72
Abad-Ali <abad02597@gmail.com>
2026-05-07 14:43:15 +0530

    Make explore page responsive

--- frontend/app/(main)/explore/page.js
+++ frontend/app/(main)/explore/page.js
@@ -7,15 +7,15 @@ import React from 'react'
 
 const explorePage = () => {
   return (
-    <div className='pt-[10vh] text-white pl-64 min-h-screen'>
+    <div className='pt-[7vh] lg:pt-[10vh] text-white lg:pl-64 min-h-screen'>
         <div className='flex flex-col items-center'>
             <div className="text-center mt-10">
-              <h1 className="text-4xl font-bold">Explore PDFs</h1>
-              <p className="text-gray-500 mt-2 font-medium">
+              <h1 className="text-3xl lg:text-4xl font-bold">Explore PDFs</h1>
+              <p className="text-gray-500 mt-2 text-[15px] lg:text-[17px] font-medium">
                 Discover recently uploaded documents from our community.
               </p>
             </div>
-            <Link href='/search'><div className="relative w-sm mt-5 mb-10">
+            <Link href='/search'><div className="relative w-xs sm:w-sm mt-5 mb-10">
               <Input placeholder="Search PDFs using title" className="pr-10 rounded-full bg-white border-2 border-blue-700 py-5 font-semibold italic"/>
               <SearchIcon strokeWidth={3} className="absolute right-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
             </div></Link>
