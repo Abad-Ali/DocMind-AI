4507c0201c24b4b072808fe4aa1a9e85d082bb73
Abad-Ali <abad02597@gmail.com>
2026-05-17 01:22:38 +0530

    Middleware not working properly

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,29 +1,29 @@
-// middleware.js
-import { NextResponse } from 'next/server'
+// // middleware.js
+// import { NextResponse } from 'next/server'
 
-export function middleware(request) {
-  const token = request.cookies.get('token')?.value
+// export function middleware(request) {
+//   const token = request.cookies.get('token')?.value
 
-  const pathname = request.nextUrl.pathname
+//   const pathname = request.nextUrl.pathname
 
-  // public routes
-  const publicRoutes = ['/login', '/signup', '/signup/verify-email']
+//   // public routes
+//   const publicRoutes = ['/login', '/signup', '/signup/verify-email']
 
-  const isPublicRoute = publicRoutes.includes(pathname)
+//   const isPublicRoute = publicRoutes.includes(pathname)
 
-  // if not logged in and trying to access protected page
-  if (!token && !isPublicRoute) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+//   // if not logged in and trying to access protected page
+//   if (!token && !isPublicRoute) {
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
 
-  // if logged in and trying to access login page
-  if (token && pathname === '/login') {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+//   // if logged in and trying to access login page
+//   if (token && pathname === '/login') {
+//     return NextResponse.redirect(new URL('/', request.url))
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: ['/', '/explore', '/search', '/dashboard', '/profile', '/profile/edit', '/pdf/:path*'],
-}
+// export const config = {
+//   matcher: ['/', '/explore', '/search', '/dashboard', '/profile', '/profile/edit', '/pdf/:path*'],
+// }
